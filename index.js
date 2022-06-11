3f0e4c57f53e3cca68ad94ea3b49742a30be1e34
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-11 16:56:42 +0300

    Arithmetic functions with the declared variables

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ let num1 = 31;
 let num2 = 2;
 let num3 = 3;
 let num4 = 12;
+
+
+let multiply = num1 * num2;
+let random = Math.random();
+let mod = num4 / num3;
+let max = Math.max(1, 2, 3, 6, 20);
